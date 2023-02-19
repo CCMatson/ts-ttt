746573f870de830175cd6239c15dcd2025c86c33
CCMatson <catherine.matson@gmail.com>
2023-02-19 11:53:33 -0800

    add winning combos

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5], 
+  [6, 7, 8], 
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 3, 6],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
 let turn: number, winner: boolean, tie: boolean, board: number[]
 
 function init() {
