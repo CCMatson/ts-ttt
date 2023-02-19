7fa3a567c6530b69bac5494b0efb45898cfd703c
CCMatson <catherine.matson@gmail.com>
2023-02-19 11:32:33 -0800

    add html

---
+++ js/app.js
@@ -0,0 +1,10 @@
+"use strict";
+let turn, winner, tie, board;
+function init() {
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    turn = 1;
+    winner = false;
+    tie = false;
+    console.log(turn);
+}
+init();
